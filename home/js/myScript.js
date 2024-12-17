1f2356875739fbc0ab2427ec564fb029d3574dd1
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2024-12-17 08:39:17 +0800

    update js

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -2,3 +2,10 @@ function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
 }
 
+function myFunction2() {
+    const element = document.getElementsByTagName("h1");
+ 
+    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+ 
+ 
+}
