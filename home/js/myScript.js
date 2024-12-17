81ac08d0c0041048d14fdb72c4e0bb60749dd83c
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2024-12-17 08:55:48 +0800

    update js

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -3,9 +3,9 @@ function myFunction() {
 }
 
 function myFunction2() {
-    const element = document.getElementsByTagName("h1");
+    const element = document.getElementsByTagName("h1")[0];
  
-    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+    document.getElementById("demo").innerHTML = 'The text in the first <h1> (index 0) is: ' + element.innerHTML;
  
  
 }
