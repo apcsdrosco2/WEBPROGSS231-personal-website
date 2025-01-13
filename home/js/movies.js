30ab3d8af62b083acbcb5431bf4690d083b31b68
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2025-01-13 13:06:22 +0800

    update

--- home/js/movies.js
+++ home/js/movies.js
@@ -2,11 +2,11 @@ const app = Vue.createApp({
     data() {
      return {
        movies: [
-         {name: 'Anne with an E', url: 'apcsdrosco2/WEBPROGSS231-personal-website/home/movies/anne.jpg'},
-         {name: 'Derry Girls', url: 'apcsdrosco2/WEBPROGSS231-personal-website/home/movies/derrygirls.jpg'},
-         {name: 'Game of Thrones', url: 'apcsdrosco2/WEBPROGSS231-personal-website/home/movies/got.jpg'},
-         {name: 'Hilda Hurricane', url: 'apcsdrosco2/WEBPROGSS231-personal-website/home/movies/hilda.jpg'},
-         {name: 'House of the Dragon', url: 'apcsdrosco2/WEBPROGSS231-personal-website/home/movies/hotd.jpg'},
+         {name: 'Anne with an E', url: 'https://raw.githubusercontent.com/apcsdrosco2/WEBPROGSS231-personal-website/home/movies/anne.jpg'},
+         {name: 'Derry Girls', url: 'https://raw.githubusercontent.com/apcsdrosco2/WEBPROGSS231-personal-website/home/movies/derrygirls.jpg'},
+         {name: 'Game of Thrones', url: 'https://raw.githubusercontent.com/apcsdrosco2/WEBPROGSS231-personal-website/home/movies/got.jpg'},
+         {name: 'Hilda Hurricane', url: 'https://raw.githubusercontent.com/apcsdrosco2/WEBPROGSS231-personal-website/home/movies/hilda.jpg'},
+         {name: 'House of the Dragon', url: 'https://raw.githubusercontent.com/apcsdrosco2/WEBPROGSS231-personal-website/home/movies/hotd.jpg'},
        ]
      }
     }
