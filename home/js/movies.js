98494616a45c205ecb2f020f9c4aa0e83c49e0cc
apcsdrosco2 <suzannemariedebelenrosco@gmail.com>
2025-01-13 13:15:46 +0800

    test

--- home/js/movies.js
+++ home/js/movies.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
      return {
        movies: [
-         {name: 'Anne with an E', url: 'https://github.com/apcsdrosco2/WEBPROGSS231-personal-website/blob/30ab3d8af62b083acbcb5431bf4690d083b31b68/home/movies/anne.jpg'},
+         {name: 'Anne with an E', url: 'movies/anne.jpg'},
          {name: 'Derry Girls', url: 'https://github.com/apcsdrosco2/WEBPROGSS231-personal-website/blob/30ab3d8af62b083acbcb5431bf4690d083b31b68/home/movies/derrygirls.jpgg'},
          {name: 'Game of Thrones', url: 'https://github.com/apcsdrosco2/WEBPROGSS231-personal-website/blob/30ab3d8af62b083acbcb5431bf4690d083b31b68/home/movies/got.jpg'},
          {name: 'Hilda Hurricane', url: 'https://github.com/apcsdrosco2/WEBPROGSS231-personal-website/blob/30ab3d8af62b083acbcb5431bf4690d083b31b68/home/movies/hilda.jpg'},
